3814e48bebbf62913205b0bfb2083771826b01b8
orest <orestis_94@hotmail.com>
2021-04-07 21:22:47 +0300

    Initial commit

---
+++ android/app/src/main/java/com/resurrectiongarage/MainActivity.java
@@ -0,0 +1,6 @@
+package com.resurrectiongarage;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
---
+++ lib/utilities/keys.dart
@@ -0,0 +1,1 @@
+const String API_KEY = 'AIzaSyBYDkpX-pRZ-L4_9Ksi7uV27fp-O6pw02k';
